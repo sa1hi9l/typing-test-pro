cc37f92cf88d8e1b1e93351dcba524e4645ecd62
Sahil <sahil@Stcs-MacBook-Pro.local>
2026-01-06 11:00:48 +0530

    chore: remove JPA until database phase

---
+++ src/main/java/com/example/typingtest/TypingTestProApplication.java
@@ -0,0 +1,13 @@
+package com.example.typingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypingTestProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypingTestProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/typingtest/TypingTestProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.typingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypingTestProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
